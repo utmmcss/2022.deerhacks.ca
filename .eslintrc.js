fbfdb56e53b308b4e9bedde2e366c496175161c2
Shawn Hu <shawnhu97@gmail.com>
2022-03-22 20:07:42 -0400

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['next', 'next/core-web-vitals', 'airbnb', 'airbnb-typescript'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
+  rules: {
+    'import/newline-after-import': ['error', { count: 1 }],
+    'object-curly-newline': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/prop-types': 'off',
+    'implicit-arrow-linebreak': 0,
+    'function-paren-newline': 0,
+    '@typescript-eslint/no-unused-vars': 1,
+    'no-confusing-arrow': 0,
+    'operator-linebreak': 0,
+    'arrow-body-style': 0,
+    'react/no-unescaped-entities': 0,
+    'react/function-component-definition': 0,
+  },
+  parser: '@typescript-eslint/parser',
+};
