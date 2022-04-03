433bb3ac2bed60d5570e3557c45ce5795caa438b
Shrey Vakil <shrey.vakil@mail.utoronto.ca>
2022-04-03 16:26:17 -0400

    Added basic footer

--- components/Sections/FooterSection.tsx
+++ components/Sections/FooterSection.tsx
@@ -8,9 +8,25 @@ interface IProps {
 
 const FooterSection: FC<IProps> = ({ isMobile }) => {
   return (
-    <ParallaxLayer offset={2} className="footer-section">
-      <Image src="/footer-bush.svg" width={1834 * 2} height={527 * 2} priority />
-    </ParallaxLayer>
+    <div className="footer-section">
+      <ParallaxLayer offset={2} className="footer-section">
+        <Image src="/footer-bush.svg" width={1834 * 2} height={527 * 2} priority />
+      </ParallaxLayer>
+      <ParallaxLayer offset={2} speed={3.5}>
+        <div className="content-container">
+          <div className="get-in-touch">
+            <h1>Get In touch</h1>
+            <p>mcss@utmsu.ca</p>
+            <p>3359 Mississauga Rd, Mississauga, ON, Canada</p>
+          </div>
+          <div className="social">
+            <p>Github</p>
+            <p>Instagram</p>
+            <p>Discord</p>
+          </div>
+        </div>
+      </ParallaxLayer>
+    </div>
   );
 };
 
