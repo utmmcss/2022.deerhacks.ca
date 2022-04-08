8cdf07f030151549ad6a711847ffc7e037bc6f01
Shawn Hu <shawnhu97@gmail.com>
2022-04-07 22:41:55 -0400

    added sun

--- components/Sections/FooterSection.tsx
+++ components/Sections/FooterSection.tsx
@@ -10,33 +10,40 @@ interface IProps {
 
 const FooterSection: FC<IProps> = ({ isMobile }) => {
   return (
-    <ParallaxLayer offset={2} className="footer-section">
-      <Image src="/footer-bushes.svg" width={1834 * 10} height={194 * 10} priority />
-      <div className="content-container">
-        <div className="get-in-touch">
-          <h2>Get In touch</h2>
-          <p>mcss@utmsu.ca</p>
-          <p>3359 Mississauga Rd, Mississauga, ON, Canada</p>
+    <>
+      <ParallaxLayer offset={2.63} speed={-3}>
+        <div className="flex justify-end pr-10">
+          <Image src="/sun.svg" width={154} height={154} priority />
         </div>
-        <div>
-          <h2>Social Media</h2>
-          <div className="media-icons">
-            <a href="https://github.com/utmmcss">
-              <GitHubIcon fontSize={isMobile ? 'medium' : 'large'} />
-              <span>Github</span>
-            </a>
-            <a href="https://www.instagram.com/utmmcss/?hl=en">
-              <InstagramIcon fontSize={isMobile ? 'medium' : 'large'} />
-              <span>Instagram</span>
-            </a>
-            <a href="https://discord.gg/t477RN2q7N">
-              <Image src="/discord.svg" width={isMobile ? 24 : 35} height={isMobile ? 24 : 35} />
-              <span>Discord</span>
-            </a>
+      </ParallaxLayer>
+      <ParallaxLayer offset={2} className="footer-section">
+        <Image src="/footer-bushes.svg" width={1834 * 10} height={194 * 10} priority />
+        <div className="content-container">
+          <div className="get-in-touch">
+            <h2>Get In touch</h2>
+            <p>mcss@utmsu.ca</p>
+            <p>3359 Mississauga Rd, Mississauga, ON, Canada</p>
+          </div>
+          <div>
+            <h2>Social Media</h2>
+            <div className="media-icons">
+              <a href="https://github.com/utmmcss">
+                <GitHubIcon fontSize={isMobile ? 'medium' : 'large'} />
+                <span>Github</span>
+              </a>
+              <a href="https://www.instagram.com/utmmcss/?hl=en">
+                <InstagramIcon fontSize={isMobile ? 'medium' : 'large'} />
+                <span>Instagram</span>
+              </a>
+              <a href="https://discord.gg/t477RN2q7N">
+                <Image src="/discord.svg" width={isMobile ? 24 : 35} height={isMobile ? 24 : 35} />
+                <span>Discord</span>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </ParallaxLayer>
+      </ParallaxLayer>
+    </>
   );
 };
 
