1f9ca444e101d99ee99db6271cbe2a99e7893ecb
Shawn Hu <shawnhu97@gmail.com>
2022-04-07 22:24:41 -0400

    fixed footer styling for desktop

--- components/Sections/FooterSection.tsx
+++ components/Sections/FooterSection.tsx
@@ -10,43 +10,33 @@ interface IProps {
 
 const FooterSection: FC<IProps> = ({ isMobile }) => {
   return (
-    <div className="footer-section">
-      <ParallaxLayer offset={2} className="footer-section">
-        <Image src="/footer-bush.svg" width={1834 * 2} height={375 * 2} priority />
-      </ParallaxLayer>
-      <ParallaxLayer offset={2} speed={3.5}>
-        <div className="content-container">
-          <div className="get-in-touch">
-            <h1>Get In touch</h1>
-            <p>mcss@utmsu.ca</p>
-            <p>3359 Mississauga Rd, Mississauga, ON, Canada</p>
-          </div>
-          <div className="social">
-            <h1>Social Media</h1>
-            <div className="icons">
-              <p>
-                <a href="https://github.com/utmmcss">
-                  <GitHubIcon fontSize="large" />
-                  Github
-                </a>
-              </p>
-              <p>
-                <a href="https://www.instagram.com/utmmcss/?hl=en">
-                  <InstagramIcon fontSize="large" />
-                  Instagram
-                </a>
-              </p>
-              <p>
-                <a href="https://www.instagram.com/utmmcss/?hl=en">
-                  <GitHubIcon fontSize="large" />
-                  Discord
-                </a>
-              </p>
-            </div>
+    <ParallaxLayer offset={2} className="footer-section">
+      <Image src="/footer-bushes.svg" width={1834 * 10} height={194 * 10} priority />
+      <div className="content-container">
+        <div className="get-in-touch">
+          <h2>Get In touch</h2>
+          <p>mcss@utmsu.ca</p>
+          <p>3359 Mississauga Rd, Mississauga, ON, Canada</p>
+        </div>
+        <div>
+          <h2>Social Media</h2>
+          <div className="media-icons">
+            <a href="https://github.com/utmmcss">
+              <GitHubIcon fontSize="large" />
+              <span>Github</span>
+            </a>
+            <a href="https://www.instagram.com/utmmcss/?hl=en">
+              <InstagramIcon fontSize="large" />
+              <span>Instagram</span>
+            </a>
+            <a href="https://discord.gg/t477RN2q7N">
+              <Image src="/discord.svg" width={35} height={35} />
+              <span>Discord</span>
+            </a>
           </div>
         </div>
-      </ParallaxLayer>
-    </div>
+      </div>
+    </ParallaxLayer>
   );
 };
 
