c07e57c0d150f97638dc5c6efa892ac714e4196f
Shrey Vakil <shrey.vakil@mail.utoronto.ca>
2022-04-13 22:37:42 -0400

    Added qasection

--- components/Sections/QAndASection.tsx
+++ components/Sections/QAndASection.tsx
@@ -5,49 +5,51 @@ import { ParallaxLayer } from '@react-spring/parallax';
 
 const QAndASection: FC = () => {
   return (
-    <ParallaxLayer offset={1.8}>
-      <h1>Frequently Asked Questions</h1>
-      <div>
-        <details open>
-          <summary>Question Test</summary>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-            eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-            obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-          </div>
-        </details>
-        <details>
-          <summary>Question Test</summary>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-            eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-            obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-          </div>
-        </details>
-        <details>
-          <summary>Question Test</summary>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-            eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-            obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-          </div>
-        </details>
-        <details>
-          <summary>Question Test</summary>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-            eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-            obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-          </div>
-        </details>
-        <details>
-          <summary>Question Test</summary>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-            eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-            obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-          </div>
-        </details>
+    <ParallaxLayer offset={1.8} className="qasection">
+      <div className="container">
+        <h1 className="text-white text-5xl text-center mb-10">Frequently Asked Questions</h1>
+        <div>
+          <details open>
+            <summary>Question Test</summary>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
+              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
+              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
+            </div>
+          </details>
+          <details>
+            <summary>Question Test</summary>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
+              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
+              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
+            </div>
+          </details>
+          <details>
+            <summary>Question Test</summary>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
+              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
+              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
+            </div>
+          </details>
+          <details>
+            <summary>Question Test</summary>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
+              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
+              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
+            </div>
+          </details>
+          <details>
+            <summary>Question Test</summary>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
+              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
+              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
+            </div>
+          </details>
+        </div>
       </div>
     </ParallaxLayer>
   );
