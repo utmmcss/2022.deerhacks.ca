65d0e31c28885c7cf420ffa1de482aa85a030eec
Shawn Hu <shawnhu97@gmail.com>
2022-04-15 17:03:56 -0400

    updated q and a section logic

--- components/Sections/QAndASection.tsx
+++ components/Sections/QAndASection.tsx
@@ -1,54 +1,57 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { ParallaxLayer } from '@react-spring/parallax';
 
 const QAndASection: FC = () => {
+  const [summaryDetailIndex, setSummaryDetailIndex] = useState(0);
+
+  const summaryDetails = [
+    {
+      title: 'Question Test 1',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur, obcaecati facilis mollitia maxime? Asperiores eius at dolores!',
+    },
+    {
+      title: 'Question Test 2',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur, obcaecati facilis mollitia maxime? Asperiores eius at dolores!',
+    },
+    {
+      title: 'Question Test 3',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur, obcaecati facilis mollitia maxime? Asperiores eius at dolores!',
+    },
+    {
+      title: 'Question Test 4',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur, obcaecati facilis mollitia maxime? Asperiores eius at dolores!',
+    },
+    {
+      title: 'Question Test 5',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur, obcaecati facilis mollitia maxime? Asperiores eius at dolores!',
+    },
+  ];
+
   return (
     <ParallaxLayer offset={1.8} className="qasection">
       <div className="container">
         <h1 className="text-white text-5xl text-center mb-10">Frequently Asked Questions</h1>
         <div>
-          <details open>
-            <summary>Question Test</summary>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-            </div>
-          </details>
-          <details>
-            <summary>Question Test</summary>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-            </div>
-          </details>
-          <details>
-            <summary>Question Test</summary>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-            </div>
-          </details>
-          <details>
-            <summary>Question Test</summary>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-            </div>
-          </details>
-          <details>
-            <summary>Question Test</summary>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas distinctio minus
-              eveniet sed numquam praesentium earum necessitatibus laborum possimus libero tenetur,
-              obcaecati facilis mollitia maxime? Asperiores eius at dolores!
-            </div>
-          </details>
+          {summaryDetails.map(({ title, content }, index) => (
+            <details
+              key={title}
+              open={summaryDetailIndex === index}
+              onClick={(e) => {
+                e.preventDefault();
+                setSummaryDetailIndex(index);
+              }}
+            >
+              <summary>{title}</summary>
+              <div>{content}</div>
+            </details>
+          ))}
         </div>
       </div>
     </ParallaxLayer>
